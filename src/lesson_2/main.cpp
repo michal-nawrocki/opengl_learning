9b7e4ddb5b79d4259f3840a5822103087d4168f8
mxn672 <mxn672@Student.bham.ac.uk>
2021-04-24 21:17:45 +0200

    [lesson_2] Start window in fullscreen mode + draw square from lesson_1

--- src/lesson_2/main.cpp
+++ src/lesson_2/main.cpp
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <fstream>
 #include <stdarg.h>
 #include <stdio.h>
 #include <string>
@@ -8,6 +9,24 @@
 #include <GLFW/glfw3.h>
 
 #define GL_LOG_FILE "gl.log"
+
+// Set Triangle coordinates
+GLfloat points[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    -0.5f, 0.5f, 0.0f,
+
+    -0.5f, 0.5f, 0.0f,
+    0.5f, 0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+
+};
+
+// Util tool for reading shader programs
+std::string* read_shader_program(std::string filepath){
+    std::ifstream file_stream(filepath);
+    return new std::string(std::istreambuf_iterator<char>(file_stream), std::istreambuf_iterator<char>());
+}
 
 bool restart_gl_log(){
     // Handle IO 
@@ -111,10 +130,8 @@ int main(){
     assert(restart_gl_log());
     gl_log("Starting GLFW\n%s\n", glfwGetVersionString());
 
-    // Set error call-back function
+    // Setup GLFW
     glfwSetErrorCallback(glfw_error_callback);
-
-    // Initialise GLFW
     if(!glfwInit()){
         fprintf(stderr, "ERROR: could not start GLFW3\n");
         return 1;
@@ -126,7 +143,19 @@ int main(){
     glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window  = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
+    // Use Anti-Aliasing
+    glfwWindowHint(GLFW_SAMPLES, 4);
+
+    // Setup fullscreen mode
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* video_mode = glfwGetVideoMode(monitor);
+    GLFWwindow* window  = glfwCreateWindow(
+        video_mode->width,
+        video_mode->height,
+        "Hello Triangle",
+        monitor,
+        NULL
+    );
     if(!window){
         fprintf(stderr, "ERROR: could not open window with GLFW3\n");
         glfwTerminate();
@@ -149,6 +178,37 @@ int main(){
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
 
+    // Setup buffer
+    GLuint vbo = 0;
+    glGenBuffers (1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+    // Setup Vertex Attribute Object
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    // Load Shaders
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    const char* vertex_shader = read_shader_program("shaders/test.vert")->c_str();
+    glShaderSource(vs, 1, &vertex_shader, NULL);
+    glCompileShader(vs);
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    const char* fragment_shader = read_shader_program("shaders/test.frag")->c_str();
+    glShaderSource(fs, 1, &fragment_shader, NULL);
+    glCompileShader(fs);
+
+    // Combine Shaders into Shader Program
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vs);
+    glAttachShader(shader_program, fs);
+    glLinkProgram(shader_program);
+
     // Draw loop
     while(!glfwWindowShouldClose(window)){
         // Clear draw surface
@@ -157,12 +217,23 @@ int main(){
         // Set background color to grey
         glClearColor(0.5, 0.5, 0.5, 1.0);
 
+        // Fillament of the triangle area bound by vertex array
+        glUseProgram(shader_program);
+
+        // Specify the bounds and draw
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, sizeof(points)/3);
+
         // Fetch input events
         glfwPollEvents();
 
+        // Make Esc key close window
+        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_ESCAPE)){
+            glfwSetWindowShouldClose(window, 1);
+        }
+
         // Display the surface
         glfwSwapBuffers(window);
-
     }
 
     // Cleanup and exit
